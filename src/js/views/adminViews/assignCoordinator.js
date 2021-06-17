6a555dbaf80007d073a96cf3263368002a4c3f19
Rogerhz <rogerhernandez0260@gmail.com>
2021-06-17 19:43:06 -0400

    assign coordinator changes

--- src/js/views/adminViews/assignCoordinator.js
+++ src/js/views/adminViews/assignCoordinator.js
@@ -5,22 +5,42 @@ import { Container, Row, Col, Form, Button, option} from "react-bootstrap";
 
 export const assignCoordinator = () => {
 	return (
-		<div>
+		<Container>
+            <Row>
 			<h2 className="titulo"> asignar Coordinador </h2>
-            <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Email@example.com" />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Label>Asignar Roll</Form.Label>
-                    <Form.Control as="select">
-                    <option>Coordinador</option>
-                    <option>Profesor</option>
-                    </Form.Control>
-                </Form.Group>
-            </Form>
-		</div>
+            </Row>
+            <Row>
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Email@example.com" onKeyPress/>
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Asignar Roll</Form.Label>
+                        <Form.Control as="select">
+                        <option>Coordinador</option>
+                        <option>Profesor</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
+            </Row>
+            <Row>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                    <Card.Body>
+                        <Card.Title>Nombre del profesor</Card.Title>
+                        <Card.Text>
+                        informacion del profesor 
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>Carrera</ListGroupItem>
+                        <ListGroupItem>materias</ListGroupItem>
+                        <ListGroupItem></ListGroupItem>
+                    </ListGroup>
+                </Card>
+            </Row>
+		</Container>
 	);
 };
 
