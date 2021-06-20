77021a13ecf80460746b2e732056b64445d58fb8
Rogerhz <rogerhernandez0260@gmail.com>
2021-06-20 18:53:31 -0400

    Views

---
+++ src/js/views/graphicsViews/graphics.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const graphics = () => {
+	return (
+		<>
+        </>
+	);
+};
+
+export default graphics;
---
+++ src/js/views/reportViews/report.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const report = () => {
+	return (
+		<>
+        </>
+	);
+};
+
+export default report;
