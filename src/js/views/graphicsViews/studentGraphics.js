06bdf3667c174abd751d024597a1afa9e65b42aa
namidabh <bphb26@gmail.com>
2021-06-24 02:52:30 +0000

    studentgraphic

--- src/js/views/graphicsViews/studentGraphics.js
+++ src/js/views/graphicsViews/studentGraphics.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React {useState, useEffect, useContext} from "react";
 import { Container, Row, Button } from "react-bootstrap";
-
+import {Bar} from "react-chartjs-2";
 import { Context } from "../../store/appContext";
 
 export const StudentGraphics = () => {
-	return (
+	const { store, actions } = useContext(Context);
+	const [StudentInfo, setStudentInfo] = useState();
+	const getInfo = async () => {
+		const info = await actions.getAllElementInfo();
+		const response = await actions.getInfo("Info", info);
+	};
+
+	useEffect(_ => {
+		const getStudentInfo = async _ => {
+			const aux = await actions.getAllElementInfo("students");
+			const graphicInfo = aux.reduce((accum, element) => {
+				return element.nacionality;
+			}, []);
+			setStudentInfo(aux);
+		};
+		getStudentInfo();
+	}, []);
+     return (
 		<Container fluid className="d-flex align-items-center bg-primary p-5 h-100">
 			<Container className="fileWrapper align-items-center bg-light border border-dark rounded-lg p-4">
 				<Row className="justify-content-center h-25">
@@ -12,8 +29,7 @@ export const StudentGraphics = () => {
 				</Row>
 				<Row className="justify-content-center h-25">
 					<div className="custom-file m-auto">
-						<input type="file" className="custom-file-input" name="myFile" />
-						<label className="custom-file-label m-auto w-75">{}</label>
+						
 					</div>
 				</Row>
 				<Row className="h-25">
